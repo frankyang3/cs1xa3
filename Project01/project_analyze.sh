d4595f4079fa3c8c2e83ee1a084cf6b5af209250
frankyang3 <frankyang3@gmail.com>
2020-02-26 23:05:26 +0000

    Save before accidents uh oh

--- Project01/project_analyze.sh
+++ Project01/project_analyze.sh
@@ -3,7 +3,7 @@
 # Initial Menu
 
 PS3='Select the feature you would like to use (Enter the corresponding number)'
-select fet in "FIXME Log" "Checkout Latest Merge" "File Size List" "File Type Count" "Quit"
+select fet in "FIXME Log" "Checkout Latest Merge" "File Size List" "File Type Count" "Switch to Executable" "Backup and Delete" "Quit" 
 do
 	case $fet in
 		"FIXME Log")
@@ -24,6 +24,32 @@ do
 			echo "Please input an extension (no . in front), this will output files with that extension"
 			read;
 			echo "There are $(ls -l | grep ".*\.${REPLY}$" | wc -l) .${REPLY} files in the repo"
+			;;
+		"Switch to Executable")
+			echo "Please type in either change or restore: "
+			>  permissions.log
+			read;
+			if [ "$REPLY" == 'change' ]; then
+				find .. -type f -name "*.sh" | [ -w xargs ] && xargs chmod u+x
+		        elif ["$REPLY" == 'restore' ]; then
+				echo "restore"
+			else
+				echo "Wrong Input"
+			fi		
+			;;
+		"Backup and Delete")
+			echo "Please type in either backup or restore"
+			[ -d "backup" ] && rm -r backup; mkdir backup || mkdir backup
+			read;
+			if [ "$REPLY" == 'backup' ]; then
+                                cp `find .. -type f -name "*.tmp"` backup
+				rm `find .. -type f -name "*.tmp" -not -path "./backup/*"`
+                        elif ["$REPLY" == 'restore' ]; then
+                                echo "restore"
+                        else
+                                echo "Wrong Input"
+                        fi
+
 			;;
 		"Quit")
 			#Breaks the select, quits
